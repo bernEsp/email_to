14c99070c650322243a5ad4ba3eceeb4215dfe90
Bernardo Galindo <bernardo466@gmail.com>
2014-11-04 15:41:05 -0600

    complete routes test

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resource :emails
+  match 'send', to: 'emails#create', via:[:post]
+  root 'emails#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- test/routes/emails_test.rb
+++ test/routes/emails_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class EmailsRouteTest < ActionDispatch::IntegrationTest
   test "should route to send via post" do
-    assert_routing '/send', controller: "emails", action: "create"
+    assert_routing({method:'post', path: 'send'}, {controller: "emails", action: "create"})
+  end
+
+  test "index should response" do
+    assert_routing '/', controller: "emails", action: "index"
   end
 end
